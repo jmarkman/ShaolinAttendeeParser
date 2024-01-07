64ba1fd933aac831db82d1dbf6dfdc5514c19142
jmarkman <jmarkman446@gmail.com>
2024-01-07 13:31:53 -0500

    Add project files.

---
+++ ShaolinAttendeeParser/AttendeeParser.cs
@@ -0,0 +1,36 @@
+﻿using CsvHelper;
+using System.Globalization;
+using System.Management.Automation;
+
+namespace ShaolinAttendeeParser
+{
+    [Cmdlet(VerbsData.Export, "ShaolinExamPlayers")]
+    public class AttendeeParser : PSCmdlet
+    {
+        private string outputFile;
+
+        [Parameter(Position = 0)]
+        public string InputFilepath { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var monthYear = DateTime.Now.ToString("MMMM yyyy");
+
+            outputFile = Path.Combine(desktopPath, $"Shaolin Exam - {monthYear}.csv");
+        }
+
+        protected override void ProcessRecord()
+        {
+            using var reader = new StreamReader(InputFilepath);
+            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var records = csvReader.GetRecords<ExamPlayer>();
+
+            using var writer = new StreamWriter(outputFile);
+            using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            csvWriter.WriteRecords(records);
+        }
+    }
+}
---
+++ ShaolinAttendeeParser/ExamPlayer.cs
@@ -0,0 +1,19 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace ShaolinAttendeeParser
+{
+    internal class ExamPlayer
+    {
+        [Name("Short GamerTag")]
+        public string Name { get; set; }
+
+        [Index(42)]
+        public string Rank { get; set; }
+
+        [Index(43)]
+        public string IsFirstTournament { get; set; }
+
+        [Index(44)]
+        public string Goals { get; set; }
+    }
+}
